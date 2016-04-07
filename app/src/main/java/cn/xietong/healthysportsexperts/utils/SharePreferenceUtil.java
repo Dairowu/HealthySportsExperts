b8483e052a445fcaadbe564e98c7a579ced897f0
denghewenai <1208612579@qq.com>
2016-04-07 17:35:00 +0800

    2016/4/7 邓贺文，图片裁剪功能，导入了第三方类库有（UIL、rxJava 、rxAndroid、ButterKnife、scissors）

--- app/src/main/java/cn/xietong/healthysportsexperts/utils/SharePreferenceUtil.java
+++ app/src/main/java/cn/xietong/healthysportsexperts/utils/SharePreferenceUtil.java
@@ -14,6 +14,7 @@ public class SharePreferenceUtil {
 	private SharedPreferences mSharedPreferences;
 	private static SharedPreferences.Editor editor;
 
+
 	public SharePreferenceUtil(Context context, String name) {
 		mSharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE);
 		editor = mSharedPreferences.edit();
