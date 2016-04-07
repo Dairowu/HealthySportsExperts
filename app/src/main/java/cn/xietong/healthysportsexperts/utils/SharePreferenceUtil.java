462a0bef111cba31bfb830f4ffea4a530de5a4ff
denghewenai <1208612579@qq.com>
2016-04-07 17:38:36 +0800

    测试

--- app/src/main/java/cn/xietong/healthysportsexperts/utils/SharePreferenceUtil.java
+++ app/src/main/java/cn/xietong/healthysportsexperts/utils/SharePreferenceUtil.java
@@ -14,7 +14,6 @@ public class SharePreferenceUtil {
 	private SharedPreferences mSharedPreferences;
 	private static SharedPreferences.Editor editor;
 
-
 	public SharePreferenceUtil(Context context, String name) {
 		mSharedPreferences = context.getSharedPreferences(name, Context.MODE_PRIVATE);
 		editor = mSharedPreferences.edit();
